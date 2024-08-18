ca244e411331482c20232efc7329b7fd2df7a209
Cornel Stoica <cornel.stoica@oit.edu>
2024-08-17 18:53:20 -0700

    reverted li highlight logic, added accessability options

--- src/Components/tasklist.jsx
+++ src/Components/tasklist.jsx
@@ -8,10 +8,20 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import "./tasklist.css";
 
-function TaskList() {
+function ToDoTaskList() {
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState("");
-    const [highlightedIndex, setHighlightedIndex] = useState("");
+    const [highlightedIndex, setHighlightedIndex] = useState(null);
+
+    useEffect(() => {
+        if (highlightedIndex !== null) {
+            const timer = setTimeout(() => {
+                setHighlightedIndex(null);
+            }, 500);
+
+            return () => clearTimeout(timer);
+        }
+    }, [highlightedIndex]);
 
     function handleInputChange(event) {
         setNewTask(event.target.value);
@@ -29,7 +39,7 @@ function TaskList() {
         setTasks(updatedTasks);
     }
 
-    function moveTaskUp(index, classname) {
+    function moveTaskUp(index) {
         if (index > 0) {
             const updatedTasks = [...tasks];
             [updatedTasks[index], updatedTasks[index - 1]] = [
@@ -37,11 +47,11 @@ function TaskList() {
                 updatedTasks[index],
             ];
             setTasks(updatedTasks);
-            setHighlightedIndex(classname);
+            setHighlightedIndex(index - 1);
         }
     }
 
-    function moveTaskDown(index, classname) {
+    function moveTaskDown(index) {
         if (index < tasks.length - 1) {
             const updatedTasks = [...tasks];
             [updatedTasks[index], updatedTasks[index + 1]] = [
@@ -49,67 +59,89 @@ function TaskList() {
                 updatedTasks[index],
             ];
             setTasks(updatedTasks);
-            setHighlightedIndex(classname);
+            setHighlightedIndex(index + 1);
         }
-    } // refactor and combine with moveTaskUp
+    }
 
     return (
-        tasks && (
-            <>
-                <div className="to-do-list">
-                    <h1 className="to-do-heading">To-Do List</h1>
-                    <div className="input-and-button">
-                        <input
-                            className="input-task-box"
-                            type="text"
-                            placeholder="Enter a task..."
-                            value={newTask}
-                            onChange={handleInputChange}
-                            onKeyDown={(event) => {
-                                event.key === "Enter" ? addTask() : null;
-                            }}
-                        />
-                        <button className="add-button" onClick={addTask}>
-                            <FontAwesomeIcon icon={faPlus} />
-                        </button>
-                    </div>
-
-                    <ul className="ul-container">
-                        {tasks.map((task, index) => (
-                            <li key={index} className="li-container">
-                                {/* <span className="text">{task}</span> */}
-                                <span className={`text ${highlightedIndex}`}>
-                                    {task}
-                                </span>
-                                <button
-                                    className="move-button"
-                                    onClick={() =>
-                                        moveTaskDown(index, "moveTaskDown")
-                                    }
-                                >
-                                    <FontAwesomeIcon icon={faArrowDown} />
-                                </button>
-                                <button
-                                    className="move-button"
-                                    onClick={() =>
-                                        moveTaskUp(index, "moveTaskUp")
-                                    }
-                                >
-                                    <FontAwesomeIcon icon={faArrowUp} />
-                                </button>
-                                <button
-                                    className="delete-button"
-                                    onClick={() => deleteTask(index)}
-                                >
-                                    <FontAwesomeIcon icon={faTrash} />
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
+        <>
+            <div className="to-do-list">
+                <h1 className="to-do-heading">To-Do List</h1>
+                <div className="input-and-button">
+                    <label htmlFor="task-input" className="visually-hidden">
+                        Task:
+                    </label>
+                    <input
+                        id="task-input"
+                        className="input-task-box"
+                        type="text"
+                        placeholder="Enter a task..."
+                        value={newTask}
+                        onChange={handleInputChange}
+                        onKeyDown={(event) => {
+                            if (event.key === "Enter") addTask();
+                        }}
+                    />
+                    <label htmlFor="add-button" className="visually-hidden">
+                        Add Task
+                    </label>
+                    <button
+                        id="add-button"
+                        className="add-button"
+                        onClick={addTask}
+                    >
+                        <FontAwesomeIcon icon={faPlus} />
+                    </button>
                 </div>
-            </>
-        )
+
+                <ul className="ul-container">
+                    {tasks.map((task, index) => (
+                        <li
+                            key={index}
+                            className={
+                                highlightedIndex === index ? "highlighted" : ""
+                            }
+                        >
+                            <span className="text">{task}</span>
+                            <button
+                                className="move-button"
+                                onClick={() => moveTaskDown(index)}
+                                onKeyDown={(event) => {
+                                    if (event.key === "ArrowDown") {
+                                        moveTaskDown(index);
+                                    }
+                                }}
+                            >
+                                <FontAwesomeIcon icon={faArrowDown} />
+                            </button>
+                            <button
+                                className="move-button"
+                                onClick={() => moveTaskUp(index)}
+                                onKeyDown={(event) => {
+                                    if (event.key === "ArrowUp") {
+                                        moveTaskUp(index);
+                                    }
+                                }}
+                            >
+                                <FontAwesomeIcon icon={faArrowUp} />
+                            </button>
+                            <button
+                                className="delete-button"
+                                onClick={() => deleteTask(index)}
+                                onKeyDown={(event) => {
+                                    if (event.key === "Delete") {
+                                        deleteTask(index);
+                                    }
+                                }}
+                            >
+                                <FontAwesomeIcon icon={faTrash} />
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
     );
 }
 
-export default TaskList;
+export default ToDoTaskList;
